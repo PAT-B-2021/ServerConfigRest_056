2556a59d70c6ca6c1166a80333e4a05d8b25ee7a
hasnashab <78135389+hasnashab@users.noreply.github.com>
2022-01-03 23:09:59 +0700

    Add project files.

---
+++ ServerConfigRest_056_HasnaShabrina/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.ServiceModel;
+using ServiceRest_056_HasnaShabrina;
+using System.Threading.Tasks;
+
+
+namespace ServerConfigRest_056_HasnaShabrina
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ServiceHost hostObjek = null;
+
+            try
+            {
+                hostObjek = new ServiceHost(typeof(TI_UMY));
+                hostObjek.Open();
+                Console.WriteLine("Server ready .. ");
+                Console.ReadLine();
+                hostObjek.Close();
+            }
+            catch (Exception ex)
+            {
+                hostObjek = null;
+                Console.WriteLine(ex.Message);
+                Console.ReadLine();
+            }
+
+        }
+    }
+}
